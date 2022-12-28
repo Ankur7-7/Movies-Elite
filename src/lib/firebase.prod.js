db1e2988a9d0c1c6e59b5f7ab8713f63a8d73864
Ankur Aggarwal <77563876+Ankur7-7@users.noreply.github.com>
2022-12-28 19:25:32 +0530

    Update firebase.prod.js

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -10,12 +10,12 @@ import 'firebase/compat/auth';
 // we ned a config here 
 
 const config = {
-    apiKey: "AIzaSyDDoJR9yR6nDCJC8iGWvPPJll-FG_Ew0M4",
-    authDomain: "netflix-clone-9d1c8.firebaseapp.com",
-    projectId: "netflix-clone-9d1c8",
-    storageBucket: "netflix-clone-9d1c8.appspot.com",
-    messagingSenderId: "1022350002780",
-    appId: "1:1022350002780:web:3d830708e69e588fcb7499"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 const firebase = Firebase.initializeApp(config);
